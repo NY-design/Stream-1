9907a2a233aeb75f0c3225e54f8f0354df0dbd40
NY-design <nisha.yadav@khaitanpublicschool.com>
2026-04-23 22:10:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import streamlit as st
 #Title of App
 #st.title("My First Streamlit App")
-st.title("My Interactive App")
+st.title("Exam Score Predictor")
 #Adding text
-#st.write("Hello! Creating a simple web application using stremlit library.")
+st.write("Hello! enter hours studies to predict the exam score.")
 
 # Take User Input
-name=st.text_input("Enter your name")
-
+#name=st.text_input("Enter your name")
+hour=st.number_input("Enter age :")
 #Display a message a button is clicked
-if st.button("Submit"):
+if st.button("Predict Score"):
    st.write(f"Hello,{name}!Welcome to strem 1")
