58464730a340f345493650230e85bd73bbd88178
NY-design <nisha.yadav@khaitanpublicschool.com>
2026-04-23 22:24:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
-import streamlit as st
+#import streamlit as st
 #Title of App
 #st.title("My First Streamlit App")
-st.title("Exam Score Predictor")
+#st.title("Exam Score Predictor")
 #Adding text
-st.write("Hello! enter hours studies to predict the exam score.")
+#st.write("Hello! enter hours studies to predict the exam score.")
 
 # Take User Input
 #name=st.text_input("Enter your name")
-hours=st.number_input("Enter age :")
+#hours=st.number_input("Enter age :")
 #Display a message a button is clicked
+#if st.button("Predict Score"):
+ #  st.write(f"Hello,{hours}!Welcome to strem 1")
+import streamlit as st
+
+st.title("Exam Score Predictor")
+
+st.write("Hello! Enter hours studied to predict the exam score.")
+
+# Input
+hours = st.number_input("Enter study hours:", min_value=0.0)
+
+# Button
 if st.button("Predict Score"):
-   st.write(f"Hello,{hours}!Welcome to strem 1")
+    st.write(f"You entered: {hours} hours")
